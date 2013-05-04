9ce9dd55fa6f02986106b15eff025c736942e2e0
jwosty <jwosty@users.noreply.github.com>
2013-05-04 11:32:02 -0600

    Registered Stargate Controller as a block

--- common/block/Blocks.java
+++ common/block/Blocks.java
@@ -1,10 +1,10 @@
 package jw.spacedistortion.common.block;
 
-import cpw.mods.fml.common.registry.GameRegistry;
-import cpw.mods.fml.common.registry.LanguageRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
 
 public class Blocks {
 	private Blocks() {}
@@ -12,9 +12,14 @@ public class Blocks {
 	public static Block stargateRing = (new StargateRing(1600, 0, Material.rock))
 			.setHardness(3.0f).setStepSound(Block.soundStoneFootstep)
 			.setBlockName("stargateRing").setCreativeTab(CreativeTabs.tabBlock);
+	public static Block stargateController = (new StargateController(1601, 1, Material.rock))
+			.setHardness(3.0f).setStepSound(Block.soundStoneFootstep)
+			.setBlockName("stargateController").setCreativeTab(CreativeTabs.tabBlock);
 	
 	public static void addBlocks() {
 		GameRegistry.registerBlock(stargateRing, "stargateRing");
 		LanguageRegistry.addName(stargateRing, "Stargate Ring");
+		GameRegistry.registerBlock(stargateController, "stargateController");
+		LanguageRegistry.addName(stargateController, "Stargate Controller");
 	}
 }
