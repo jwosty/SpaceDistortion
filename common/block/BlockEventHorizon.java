ee8e84be4019e933bce4c191648a56e80d63f5cd
jwosty <jwosty@users.noreply.github.com>
2013-05-04 14:59:15 -0600

    Made event horizon block non-solid

--- common/block/BlockEventHorizon.java
+++ common/block/BlockEventHorizon.java
@@ -1,6 +1,8 @@
 package jw.spacedistortion.common.block;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.world.World;
 
 public class BlockEventHorizon extends SDBlock {
 	// The coordinate at which the textures for this block starts
@@ -9,5 +11,20 @@ public class BlockEventHorizon extends SDBlock {
 	public BlockEventHorizon(int id, int _blockIndexInTexture) {
 		super(id, _blockIndexInTexture, Material.portal);
 		blockIndexInTexture = _blockIndexInTexture;
+	}
+	
+	@Override
+	public boolean isOpaqueCube() {
+		return false;
+	}
+	
+	@Override
+	public boolean renderAsNormalBlock() {
+		return false;
+	}
+	
+	@Override
+	public AxisAlignedBB getCollisionBoundingBoxFromPool(World world, int x, int y, int z) {
+		return null;
 	}
 }
