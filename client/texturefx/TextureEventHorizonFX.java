f376321c0d87c8fe28656a81d15659fe2a40d75e
jwosty <jwosty@users.noreply.github.com>
2013-05-06 19:40:09 -0600

    Created stub class TextureEventHorizonFX

---
+++ client/texturefx/TextureEventHorizonFX.java
@@ -0,0 +1,10 @@
+package jw.spacedistortion.client.texturefx;
+
+import jw.spacedistortion.common.block.SDBlock;
+import cpw.mods.fml.client.FMLTextureFX;
+
+public class TextureEventHorizonFX extends FMLTextureFX {
+	public TextureEventHorizonFX() {
+		super(SDBlock.eventHorizon.blockIndexInTexture);
+	}
+}
