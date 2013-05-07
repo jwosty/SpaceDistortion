6ec1d7097ed76a890acdc989393475530de740ac
jwosty <jwosty@users.noreply.github.com>
2013-05-06 19:35:38 -0600

    Modified SpaceDistortion mod class to implement BaseMod

--- common/SpaceDistortion.java
+++ common/SpaceDistortion.java
@@ -1,5 +1,7 @@
 package jw.spacedistortion.common;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.src.BaseMod;
 import jw.spacedistortion.common.block.SDBlock;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
@@ -12,7 +14,7 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid="SpaceDistortion", name="Space Distortion", version="0.0.0")
-public class SpaceDistortion {
+public class SpaceDistortion extends BaseMod {
 	// The instance of this mod that Forge uses
 	@Instance("SpaceDistortion")
 	public static SpaceDistortion instance;
@@ -26,13 +28,24 @@ public class SpaceDistortion {
 	}
 	
 	@Init
+	// Not sure whether to use this or load if you use both the FML mod api or the Minecraft ModLoader api... :/
 	public void init(FMLInitializationEvent event) {
 		proxy.registerRenderers();
 		SDBlock.addBlocks();
 	}
 	
+	@Override
+	// See SpaceDistortion#init
+	public void load() {
+	}
+	
 	@PostInit
 	public void postInit(FMLPostInitializationEvent event) {
 		// Stub method
+	}
+
+	@Override
+	public String getVersion() {
+		return "Space Distortion mod v0.0.0 for Minecraft 1.4.7";
 	}
 }
