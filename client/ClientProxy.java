1ee66f1c7619e6acf4a9bdfc9d1de230e1444698
jwosty <jwosty@users.noreply.github.com>
2013-05-14 21:43:37 -0600

    Just found out that you don't need any code for animations; removing unneeded TextureEventHorizonFX and references

--- client/ClientProxy.java
+++ client/ClientProxy.java
@@ -1,15 +1,11 @@
 package jw.spacedistortion.client;
 
-import jw.spacedistortion.client.texturefx.TextureEventHorizonFX;
 import jw.spacedistortion.common.CommonProxy;
-import net.minecraft.src.ModLoader;
 import net.minecraftforge.client.MinecraftForgeClient;
 
 public class ClientProxy extends CommonProxy {
 	@Override
 	public void registerRenderers() {
 		MinecraftForgeClient.preloadTexture(TEXTURES_PNG);
-		//Minecraft.getMinecraft().renderEngine.registerTextureFX(new TextureEventHorizonFX());
-		ModLoader.addAnimation(new TextureEventHorizonFX());
 	}
 }
