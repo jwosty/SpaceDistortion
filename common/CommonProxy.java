61a1e0e42dd654052c78b5e7463f4a13d9353382
jwosty <jwosty@users.noreply.github.com>
2013-05-15 07:08:42 -0600

    Moved texture file into seperate folder and added a 2-frame animated texture for the event horizon

--- common/CommonProxy.java
+++ common/CommonProxy.java
@@ -1,7 +1,7 @@
 package jw.spacedistortion.common;
 
 public class CommonProxy {
-	public static String TEXTURES_PNG = "/jw/spacedistortion/textures.png";
+	public static String TEXTURES_PNG = "/jw/spacedistortion/texture/textures.png";
 	
 	public void registerRenderers() {
 		// Nothing here as the server doesn't render graphics!
