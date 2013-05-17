1d94e2b113196c860886f367702005e915876f71
jwosty <jwosty@users.noreply.github.com>
2013-05-16 21:24:10 -0600

    Wrote a method to test if there is a valid stargate neighboring the controller; doesn't do anything interesting yet

--- common/block/BlockStargateController.java
+++ common/block/BlockStargateController.java
@@ -1,7 +1,7 @@
 package jw.spacedistortion.common.block;
 
-import jw.spacedistortion.common.CommonProxy;
 import net.minecraft.block.material.Material;
+import net.minecraft.world.World;
 
 public class BlockStargateController extends SDBlock {
 	// The coordinate at which the textures for this block starts
@@ -11,6 +11,36 @@ public class BlockStargateController extends SDBlock {
 	public BlockStargateController(int id, int _blockIndexInTexture) {
 		super(id, Material.rock);
 		blockIndexInTexture = _blockIndexInTexture;
+	}
+	
+	// Returns the position of the first neighboring block found that is a stargate ring
+	public int[] getRelativeStargateBlocks(World world, int x, int y, int z) {
+		int[] blockPosition = null;
+		int[][] offsets = {
+				{-1, 0, 0}, {1, 0, 0},
+				{0, -1, 0}, {0, 1, 0},
+				{0, 0, -1}, {0, 0, 1}
+		};
+		search:
+		for (int i = 0; i < offsets.length; i++) {
+			int[] offset = offsets[i];
+			int bx = offset[0];
+			int by = offset[1];
+			int bz = offset[2];
+			if (world.getBlockId(bx, by, bz) != 0) {
+				blockPosition = new int[]{bx, by, bz};
+				break search;				
+			}
+		}
+		return blockPosition;
+	}
+
+	@Override
+	public void onBlockAdded(World world, int x, int y, int z) {
+		int[] stargateBlockPos = this.getRelativeStargateBlocks(world, x, y, z);
+		if (stargateBlockPos != null) {
+			world.setBlock(stargateBlockPos[0], stargateBlockPos[1], stargateBlockPos[2], SDBlock.eventHorizon.blockID);
+		}
 	}
 	
 	@Override
