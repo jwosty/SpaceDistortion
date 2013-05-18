485c681398abcf3a7f3133bea2cbca3e031d3bf7
jwosty <jwosty@users.noreply.github.com>
2013-05-18 11:59:18 -0600

    Wrote a constructor and a width field

--- StringGrid.java
+++ StringGrid.java
@@ -1,5 +1,22 @@
 package jw.spacedistortion;
 
 public class StringGrid {
-
+	private String[] strings;
+	public int width;
+	
+	public StringGrid(String... strings) {
+		this.strings = strings;
+		this.width = this.findWidth();
+	}
+	
+	private int findWidth() {
+		int w = 0;
+		for (int i = 0; i < strings.length; i++) {
+			int rowW = strings[i].length();
+			if (w < rowW) {
+				w = rowW;
+			}
+		}
+		return w;
+	}
 }
