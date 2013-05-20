b4edc915c7622acc9e32a3bb0518ac39bcdf4de8
jwosty <jwosty@users.noreply.github.com>
2013-05-20 17:03:45 -0600

    Finished implementing matchStructure (as far as I know; haven't tested yet)

--- common/block/SDBlock.java
+++ common/block/SDBlock.java
@@ -74,14 +74,26 @@ public class SDBlock extends Block {
 		return new int[]{};
 	}
 	
-	// Try to match a structure using a StringGrid and the given position
+	// Try to match a structure using a StringGrid and the given position. If the match fails, blocks will
+	// be null
 	// TODO: Implement other axes (pl. axis)
-	private int[] matchStructure(World world, int xOrigin, int yOrigin, int zOrigin, StringGrid template) {
-		int[] blocks = new int[template.width * template.height];
+	private boolean[][] matchStructure(World world, int x, int y, int z, StringGrid template) {
+		// To keep track of the found blocks, if any
+		boolean[][] blocks = new boolean[template.width][template.height];
 		match:
 		for (int xOffset = 0; xOffset < template.width; xOffset++) {
 			for (int yOffset = 0; yOffset < template.height; yOffset++) {
-				
+				int id = world.getBlockId(x + xOffset, y + yOffset, z);
+				if (template.get(xOffset, yOffset) != ' ') {
+					// Expecting this block
+					if (id == this.blockID) {
+						// We matched a block on the structure, so add it to the list
+						blocks[xOffset][yOffset] = true;
+					} else {
+						// This match evidently didn't work, so fail
+						blocks = null;
+					}
+				} // No 'else' clause as blocks not part of the structure don't matter
 			}
 		}
 		return blocks;
