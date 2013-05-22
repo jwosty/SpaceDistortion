52825daa0172eb8afa03375cbeea445dd173ec45
jwosty <jwosty@users.noreply.github.com>
2013-05-22 15:17:58 -0600

    Removed flip parameters

--- common/block/BlockStargateRing.java
+++ common/block/BlockStargateRing.java
@@ -9,13 +9,10 @@ public class BlockStargateRing extends SDBlock {
 	public BlockStargateRing(int id, int texture) {
 		super(id, texture, Material.rock);
 	}
-	
+
 	public void onBlockAdded(World world, int x, int y, int z) {
-		if (this.detectStructureAtLocation(world, new StringGrid(
-				"XX", "XX"),
-				x, y,
-				z, 1, false
-				) != null) {
+		if (this.detectStructureAtLocation(world, new StringGrid("XXX"), x, y,
+				z, -1) != null) {
 			world.setBlock(x, y, z, Block.blockEmerald.blockID);
 		}
 	}
