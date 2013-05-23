c7df5831d197fbdd71e19568890d23de77a197e1
jwosty <jwosty@users.noreply.github.com>
2013-05-23 12:19:43 -0600

    Added comment and renamed 2 parameters

--- common/tileentity/TileEntityStargateController.java
+++ common/tileentity/TileEntityStargateController.java
@@ -6,16 +6,17 @@ import net.minecraft.tileentity.TileEntity;
 public class TileEntityStargateController extends TileEntity {
 	public boolean isActivated = false;
 	public boolean isOutgoing = false;
+	// Coordinates of the destination stargate controller
 	public int xDest = 0;
 	public int yDest = 0;
 	public int zDest = 0;
 	
 	@Override
-	public void writeToNBT(NBTTagCompound tag) {
-		super.readFromNBT(tag);
+	public void writeToNBT(NBTTagCompound data) {
+		super.readFromNBT(data);
 	}
 	
-	public void readFromNBT(NBTTagCompound tag) {
-		super.readFromNBT(tag);
+	public void readFromNBT(NBTTagCompound data) {
+		super.readFromNBT(data);
 	}
 }
