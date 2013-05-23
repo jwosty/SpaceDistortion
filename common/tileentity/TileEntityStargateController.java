f41382e7e74c59a45314fdbbe9768db9a8079f54
jwosty <jwosty@users.noreply.github.com>
2013-05-23 12:15:47 -0600

    Added some fields

--- common/tileentity/TileEntityStargateController.java
+++ common/tileentity/TileEntityStargateController.java
@@ -4,6 +4,12 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 
 public class TileEntityStargateController extends TileEntity {
+	public boolean isActivated = false;
+	public boolean isOutgoing = false;
+	public int xDest = 0;
+	public int yDest = 0;
+	public int zDest = 0;
+	
 	@Override
 	public void writeToNBT(NBTTagCompound tag) {
 		super.readFromNBT(tag);
