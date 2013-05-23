7b266fad24b5d0e59f44ff6068c04bad76698a90
jwosty <jwosty@users.noreply.github.com>
2013-05-23 11:52:52 -0600

    Wrote a stub tile entity for the stargate controller block that will hold info about the gate, like its mode, the gate its connected to, etc

---
+++ common/tileentity/TileEntityStargateController.java
@@ -0,0 +1,15 @@
+package jw.spacedistortion.common.tileentity;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+
+public class TileEntityStargateController extends TileEntity {
+	@Override
+	public void writeToNBT(NBTTagCompound tag) {
+		super.readFromNBT(tag);
+	}
+	
+	public void readFromNBT(NBTTagCompound tag) {
+		super.readFromNBT(tag);
+	}
+}
