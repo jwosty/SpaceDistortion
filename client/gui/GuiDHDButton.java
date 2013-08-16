4080ef1778586c4e94b851d3ba5daa675df4f817
jwosty <jwosty@users.noreply.github.com>
2013-08-16 11:24:59 -0600

    Removed scale stuff

--- client/gui/GuiDHDButton.java
+++ client/gui/GuiDHDButton.java
@@ -27,24 +27,9 @@ public class GuiDHDButton extends GuiButton {
 			GL11.glDisable(GL11.GL_LIGHTING);
 			GL11.glEnable(GL11.GL_BLEND);
 			GL11.glBindTexture(GL11.GL_TEXTURE_2D, mc.renderEngine.getTexture(CommonProxy.GLYPHS_PNG));
-			int scale = 1;
-			this.drawTexturedModalRectWithScale(this.xPosition, this.yPosition,
+			this.drawTexturedModalRect(this.xPosition, this.yPosition,
 					this.glyphID % (GlyphSheetWidth / GlyphWidth) * GlyphWidth, this.glyphID / (GlyphSheetWidth / GlyphWidth) * GlyphHeight,
-					GlyphWidth, GlyphHeight, scale);
+					GlyphWidth, GlyphHeight);
 		}
-	}
-	
-	public void drawTexturedModalRectWithScale(int screenX, int screenY, int u, int v, int textureWidth, int textureHeight, int scale) {
-		float var7 = 0.00390625F * scale;
-		float var8 = 0.00390625F * scale;
-		int width = textureWidth / scale;
-		int height = textureHeight / scale;
-		Tessellator var9 = Tessellator.instance;
-		var9.startDrawingQuads();
-		var9.addVertexWithUV((double) (screenX + 0), (double) (screenY + height), (double) this.zLevel, (double) ((float) (u + 0) * var7), (double) ((float) (v + height) * var8));
-		var9.addVertexWithUV((double) (screenX + width), (double) (screenY + height), (double) this.zLevel, (double) ((float) (u + width) * var7), (double) ((float) (v + height) * var8));
-		var9.addVertexWithUV((double) (screenX + width), (double) (screenY + 0), (double) this.zLevel, (double) ((float) (u + width) * var7), (double) ((float) (v + 0) * var8));
-		var9.addVertexWithUV((double) (screenX + 0), (double) (screenY + 0), (double) this.zLevel, (double) ((float) (u + 0) * var7), (double) ((float) (v + 0) * var8));
-		var9.draw();
 	}
 }
