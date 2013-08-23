4cc7c7a876448640a9bdae01118e7803a0768836
jwosty <jwosty@users.noreply.github.com>
2013-08-22 21:22:50 -0600

    Changed the ModID and ModName slightly and refactored them into CommonProxy, as well as the texture path and the mod version

--- common/CommonProxy.java
+++ common/CommonProxy.java
@@ -1,10 +1,14 @@
 package jw.spacedistortion.common;
 
 public class CommonProxy {
-	public static String TEXTURES_PNG = "/jw/spacedistortion/texture/textures.png";
-	public static String GLYPHS_PNG = "/jw/spacedistortion/texture/glyphs.png";
+	public static final String MOD_ID = "spacedistortion";
+	public static final String MOD_NAME = "SpaceDistortion";
+	public static final String MOD_VERSION = "0.0.0";
+	public static final String TEX_ROOT = "/jw/spacedistortion/texture/";
+	public static final String TEXTURES_PNG = TEX_ROOT + "textures.png";
+	public static final String GLYPHS_PNG = TEX_ROOT + "glyphs.png";
 	// Looks nothing like the DHD in the show
-	public static String DHD_PNG = "/jw/spacedistortion/texture/DHD.png";
+	public static final String DHD_PNG = TEX_ROOT + "DHD.png";
 	
 	public void registerRenderers() {
 		// Nothing here as the server doesn't render graphics!
--- common/SpaceDistortion.java
+++ common/SpaceDistortion.java
@@ -11,7 +11,7 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
-@Mod(modid="SpaceDistortion", name="Space Distortion", version="0.0.0")
+@Mod(modid = CommonProxy.MOD_ID, name = CommonProxy.MOD_NAME, version = CommonProxy.MOD_VERSION)
 @NetworkMod(clientSideRequired=true, serverSideRequired=false, channels={"OutgoingWormhole"}, packetHandler=PacketHandler.class)
 public class SpaceDistortion {
 	// The instance of this mod that Forge uses
