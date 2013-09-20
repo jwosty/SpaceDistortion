76cd6b1469abf8d06e4bdd4f5421ae4ed53f6c3a
jwosty <jwosty@users.noreply.github.com>
2013-09-20 17:30:54 -0600

    Eclipse continues to insist on the formatting

--- common/block/BlockStargateController.java
+++ common/block/BlockStargateController.java
@@ -20,10 +20,21 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlockStargateController extends SDBlock {
-	public static StringGrid stargateRingShape = new StringGrid("  XXX  ",
-			" X   X ", "X     X", "X     X", "X     X", " X   X ", "  XXX  ");
+	public static StringGrid stargateRingShape = new StringGrid(
+			"  XXX  ",
+			" X   X ",
+			"X     X",
+			"X     X",
+			"X     X",
+			" X   X ",
+			"  XXX  ");
 	public static StringGrid stargateEventHorizonShape = new StringGrid(
-			"       ", "  XXX  ", " XXXXX ", " XXXXX ", " XXXXX ", "  XXX  ",
+			"       ",
+			"  XXX  ",
+			" XXXXX ",
+			" XXXXX ",
+			" XXXXX ",
+			"  XXX  ",
 			"       ");
 
 	private Icon controllerTopIcon;
@@ -71,63 +82,74 @@ public class BlockStargateController extends SDBlock {
 	}
 
 	public Tuple<Integer, Tuple<Integer, Integer>> decodeAddress(byte[] address) {
-		for (int i = 0; i < 7; i++) {
-			if (i < 6) {
-				System.out.print(address[i] + " ");
-			} else {
-				System.out.print("(" + Integer.toBinaryString(address[i])
-						+ ")\n");
+			for (int i = 0; i < 7; i++) {
+				if (i < 6) {
+					System.out.print(address[i] + " ");
+				} else {
+					System.out.print("(" + Integer.toBinaryString(address[i])
+							+ ")\n");
+				}
 			}
-		}
-		// Building base 39 numbers using powers of 3
-		int chunkX = (int) ((address[0] * 1521) + (address[1] * 39) + address[2]);
-		int chunkZ = (int) ((address[3] * 1521) + (address[4] * 39) + address[2]);
-		int last = (int) address[6];
-		// The dimension is stored in the last 2 bits of the last number/symbol
-		// (the
-		// mask is 0b11)
-		int dimension = last & 3;
-		// The sign of the x coordinate is the 4rd to last bit (the mask is
-		// 0b1000)
-		int xSign = last & 8;
-		// The sign of the z coordinate is the 3th to last bit (the mask is
-		// 0b100)
-		int zSign = last & 4;
-		if (xSign == 0) {
-			chunkX = -chunkX;
-		}
-		if (zSign == 0) {
-			chunkZ = -chunkZ;
-		}
-		System.out.println("chunkX = " + chunkX + ", chunkZ = " + chunkZ
-				+ ", dimension = " + dimension);
-		return new Tuple<Integer, Tuple<Integer, Integer>>(dimension, new Tuple(chunkX, chunkZ));
+			// Building base 39 numbers using powers of 3
+			int chunkX = (int) ((address[0] * 1521) + (address[1] * 39) + address[2]);
+			int chunkZ = (int) ((address[3] * 1521) + (address[4] * 39) + address[2]);
+			int last = (int) address[6];
+			// The dimension is stored in the last 2 bits of the last
+			// number/symbol
+			// (the
+			// mask is 0b11)
+			int dimension = last & 3;
+			// The sign of the x coordinate is the 4rd to last bit (the mask is
+			// 0b1000)
+			int xSign = last & 8;
+			// The sign of the z coordinate is the 3th to last bit (the mask is
+			// 0b100)
+			int zSign = last & 4;
+			if (xSign == 0) {
+				chunkX = -chunkX;
+			}
+			if (zSign == 0) {
+				chunkZ = -chunkZ;
+			}
+			System.out.println("chunkX = " + chunkX + ", chunkZ = " + chunkZ
+					+ ", dimension = " + dimension);
+			return new Tuple<Integer, Tuple<Integer, Integer>>(dimension,
+					new Tuple(chunkX, chunkZ));
 	}
-	
+
 	@SideOnly(Side.CLIENT)
 	public void addressReceived(byte[] address, int dhdX, int dhdY, int dhdZ) {
 		// Send the data over the wire
-		Minecraft.getMinecraft().thePlayer.sendQueue.addToSendQueue(new OutgoingWormholePacket(dhdX, dhdY, dhdZ, address).makePacket());
+		Minecraft.getMinecraft().thePlayer.sendQueue
+				.addToSendQueue(new OutgoingWormholePacket(dhdX, dhdY, dhdZ,
+						address).makePacket());
 	}
-	
+
 	/**
-	 * Activate the stargate attached to the given controller coordinates 
+	 * Activate the stargate attached to the given controller coordinates
 	 */
-	public void serverActivateStargatePair(World world, int x, int y, int z, int xDest, int yDest, int zDest) {
+	public void serverActivateStargatePair(World world, int x, int y, int z,
+			int xDest, int yDest, int zDest) {
 		Side side = FMLCommonHandler.instance().getEffectiveSide();
 		// If there's no controller block, don't continue
 		if (world.getBlockId(x, y, z) != SDBlock.stargateController.blockID) {
-			System.out.println("No controller at (" + x + ", " + y + ", " + z + ")");
+			System.out.println("No controller at (" + x + ", " + y + ", " + z
+					+ ")");
 			return;
 		}
-		DetectStructureResults stargate = this.getStargateBlocks(Minecraft.getMinecraft().theWorld, x, y, z);
+		DetectStructureResults stargate = this.getStargateBlocks(
+				Minecraft.getMinecraft().theWorld, x, y, z);
 		if (stargate == null) {
-			System.out.println("No stargate at (" + x + ", " + y + ", " + z + ")");
+			System.out.println("No stargate at (" + x + ", " + y + ", " + z
+					+ ")");
 			return;
 		}
-		Integer[] firstNeighbor = this.getNeighboringBlocks(world, x, y, z).get(0);
+		Integer[] firstNeighbor = this.getNeighboringBlocks(world, x, y, z)
+				.get(0);
 		// This is the top-left corner of the stargate ring
-		int[] origin = this.getBlockInStructure(world, firstNeighbor[0], firstNeighbor[1], firstNeighbor[2], -stargate.xOffset, stargate.yOffset, stargate.plane);
+		int[] origin = this.getBlockInStructure(world, firstNeighbor[0],
+				firstNeighbor[1], firstNeighbor[2], -stargate.xOffset,
+				stargate.yOffset, stargate.plane);
 		// Fill the center of the ring with EventHorizon blocks
 		for (int templateX = 0; templateX <= stargateEventHorizonShape.width; templateX++) {
 			for (int templateY = 0; templateY <= stargateEventHorizonShape.height; templateY++) {
@@ -135,8 +157,10 @@ public class BlockStargateController extends SDBlock {
 					int[] coords = this.getBlockInStructure(world, origin[0],
 							origin[1], origin[2], templateX, -templateY,
 							stargate.plane);
-					world.setBlock(coords[0], coords[1], coords[2], SDBlock.eventHorizon.blockID, 0, 2);
-					world.setBlockTileEntity(coords[0], coords[1], coords[2], new TileEntityEventHorizon(xDest, yDest, zDest));
+					world.setBlock(coords[0], coords[1], coords[2],
+							SDBlock.eventHorizon.blockID, 0, 2);
+					world.setBlockTileEntity(coords[0], coords[1], coords[2],
+							new TileEntityEventHorizon(xDest, yDest, zDest));
 				}
 			}
 		}
@@ -149,7 +173,8 @@ public class BlockStargateController extends SDBlock {
 	 **/
 	public DetectStructureResults getStargateBlocks(World world, int xOrigin,
 			int yOrigin, int zOrigin) {
-		List<Integer[]> neighbors = this.getNeighboringBlocks(world, xOrigin, yOrigin, zOrigin);
+		List<Integer[]> neighbors = this.getNeighboringBlocks(world, xOrigin,
+				yOrigin, zOrigin);
 		for (int i = 0; i < neighbors.size(); i++) {
 			Integer[] blockInfo = neighbors.get(i);
 			if (blockInfo[3] == SDBlock.stargateRing.blockID) {
