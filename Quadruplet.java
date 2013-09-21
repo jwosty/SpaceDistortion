8d26b24d8342faba62604fe0518c83768e9c0cbc
jwosty <jwosty@users.noreply.github.com>
2013-09-21 16:10:29 -0600

    Wrote a quick Qudruplet tuple class

---
+++ Quadruplet.java
@@ -0,0 +1,14 @@
+package jw.spacedistortion;
+
+public class Quadruplet<A, B, C, D> {
+	public A A;
+	public B B;
+	public C C;
+	public D D;
+	public Quadruplet(A A, B B, C C, D D) {
+		this.A = A;
+		this.B = B;
+		this.C = C;
+		this.D = D;
+	}
+}
