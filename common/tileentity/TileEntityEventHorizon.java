1687aea523a7845811e384be6e5d63384905d287
jwosty <jwosty@users.noreply.github.com>
2013-09-20 22:09:08 -0600

    Renamed variables

--- common/tileentity/TileEntityEventHorizon.java
+++ common/tileentity/TileEntityEventHorizon.java
@@ -5,30 +5,30 @@ import net.minecraft.tileentity.TileEntity;
 
 public class TileEntityEventHorizon extends TileEntity {
 	// Coordinates of the destination stargate controller
-	public int xDest;
-	public int yDest;
-	public int zDest;
+	public int destX;
+	public int destY;
+	public int destZ;
 	
 	public TileEntityEventHorizon(int xDest, int yDest, int zDest) {
-		this.xDest = xDest;
-		this.yDest = yDest;
-		this.zDest = zDest;
+		this.destX = xDest;
+		this.destY = yDest;
+		this.destZ = zDest;
 	}
 	
 	@Override
 	public void writeToNBT(NBTTagCompound data) {
 		super.readFromNBT(data);
-		data.setIntArray("destCoords", new int[]{xDest, yDest, zDest});
-		System.out.println("x = " + this.xDest + ", y = " + this.yDest + ", z = " + this.zDest);
+		data.setIntArray("destCoords", new int[]{destX, destY, destZ});
+		System.out.println("x = " + this.destX + ", y = " + this.destY + ", z = " + this.destZ);
 	}
 	
 	@Override
 	public void readFromNBT(NBTTagCompound data) {
 		super.readFromNBT(data);
 		int[] destCoords = data.getIntArray("destCoords");
-		xDest = destCoords[0];
-		yDest = destCoords[1];
-		zDest = destCoords[2];
-		System.out.println("x = " + this.xDest + ", y = " + this.yDest + ", z = " + this.zDest);
+		destX = destCoords[0];
+		destY = destCoords[1];
+		destZ = destCoords[2];
+		System.out.println("x = " + this.destX + ", y = " + this.destY + ", z = " + this.destZ);
 	}
 }
