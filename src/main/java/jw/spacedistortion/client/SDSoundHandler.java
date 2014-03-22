4f93afb9b3a44655ea710481d47386a828fd2e4b
jwosty <jwosty@users.noreply.github.com>
2014-03-22 13:00:58 -0600

    Refactored and added more stargate sounds

--- src/main/java/jw/spacedistortion/client/SDSoundHandler.java
+++ src/main/java/jw/spacedistortion/client/SDSoundHandler.java
@@ -1,16 +1,38 @@
 package jw.spacedistortion.client;
 
+import java.util.List;
+
 import jw.spacedistortion.common.CommonProxy;
-import net.minecraft.client.audio.PositionedSoundRecord;
-import net.minecraft.client.audio.SoundHandler;
-import net.minecraft.util.ResourceLocation;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.network.play.server.S29PacketSoundEffect;
 import net.minecraft.world.World;
 
 public class SDSoundHandler {
+	public static String ensureSoundNamePrefix(String sound) {
+		String[] parts = sound.split(":");
+		if (parts.length == 1) {
+			sound = CommonProxy.MOD_ID;
+			// Add the rest of the string back
+			for (String part : parts) {
+				sound += ":" + part;
+			}
+			
+		}
+		return sound;
+	}
+	
 	public static void playSoundAtPosition(World world, int x, int y, int z, String sound) {
 		world.playSoundEffect(
 				(double)x + 0.5D, (double)y + 0.5D, (double)z + 0.5D,
-				CommonProxy.MOD_ID + ":" + sound,
-				1.0F, 1.25F);//world.rand.nextFloat() * 0.25F + 0.75F);
+				SDSoundHandler.ensureSoundNamePrefix(sound),
+				1.0F, 1.25F);
+	}
+	
+	public static void serverPlaySoundToPlayers(List players, String sound, float volume, float pitch, double x, double y, double z) {
+		for (int i = 0; i < players.size(); i++) {
+			((EntityPlayerMP) players.get(i)).playerNetServerHandler.sendPacket(new S29PacketSoundEffect(
+					SDSoundHandler.ensureSoundNamePrefix(sound),
+					x, y, z, volume, pitch));
+		}
 	}
 }
--- src/main/java/jw/spacedistortion/common/network/packet/PacketDHDEnterGlyph.java
+++ src/main/java/jw/spacedistortion/common/network/packet/PacketDHDEnterGlyph.java
@@ -2,6 +2,7 @@ package jw.spacedistortion.common.network.packet;
 
 import io.netty.buffer.ByteBuf;
 import jw.spacedistortion.Triplet;
+import jw.spacedistortion.client.SDSoundHandler;
 import jw.spacedistortion.common.CommonProxy;
 import jw.spacedistortion.common.block.BlockStargateController;
 import jw.spacedistortion.common.block.SDBlock;
@@ -46,37 +47,13 @@ public class PacketDHDEnterGlyph implements IPacket {
 		bytes.writeInt(this.z);
 		bytes.writeByte(this.glyphID);
 	}
-
+	
 	@Override
 	public void onReceive(EntityPlayer player, Side side) {
 		if (!player.worldObj.isRemote) {
 			TileEntityStargateController tileEntity = (TileEntityStargateController) player.worldObj.getTileEntity(this.x, this.y, this.z);
 			if (this.glyphID == 39) {
-				Triplet<Integer, Integer, Integer> decodedAddress = SDBlock.stargateController.decodeAddress(tileEntity.dialingAddress);
-				int dstDimension = decodedAddress.X;
-				// Get chunk coordinates
-				int dstChunkX = decodedAddress.Y;
-				int dstChunkZ = decodedAddress.Z;
-				int[] dstCoords = BlockStargateController.getDominantController(player.worldObj, dstChunkX, dstChunkZ);
-				if (dstCoords == null) {
-					return;
-				}
-				TileEntityStargateController dstTileEntity = (TileEntityStargateController) player.worldObj.getTileEntity(dstCoords[0], dstCoords[1], dstCoords[2]);
-				
-				// Activate or deactivate the stargates if possible
-				if (dstCoords != null) {
-					switch (tileEntity.state) {
-					case READY:
-						SDBlock.stargateController.serverActivateStargatePair(player.worldObj, this.x, this.y, this.z, dstCoords[0], dstCoords[1], dstCoords[2]);
-						break;
-					case ACTIVE_OUTGOING:
-						SDBlock.stargateController.serverDeactivateStargatePair(player.worldObj, this.x, this.y, this.z, dstCoords[0], dstCoords[1], dstCoords[2]);
-						break;
-					default:
-					}
-				}
-				
-				
+				this.toggleStargatePair(player, tileEntity);
 			} else {
 				// Add another glyph into the dialing address
 				if (tileEntity.currentGlyphIndex < 7) {
@@ -84,17 +61,52 @@ public class PacketDHDEnterGlyph implements IPacket {
 					tileEntity.currentGlyphIndex++;
 				}
 				
-				// Play sounds to all players
-				for (int i = 0; i < player.worldObj.playerEntities.size(); i++) {
-					EntityPlayerMP p = (EntityPlayerMP) player.worldObj.playerEntities.get(i);
-					p.playerNetServerHandler.sendPacket(new S29PacketSoundEffect(
-							CommonProxy.MOD_ID + ":stargate.dhd.glyph",
-							(double) tileEntity.xCoord, (double) tileEntity.yCoord, (double) tileEntity.zCoord,
-							1.0F, 1.0F));
-				}				
+				// Play a glyph press sound on all clients at the stargate controller block
+				SDSoundHandler.serverPlaySoundToPlayers(
+						player.worldObj.playerEntities, "stargate.glyph", 1F, 1F,
+						(double) tileEntity.xCoord, (double) tileEntity.yCoord, (double) tileEntity.zCoord);	
 			}
 			// Send changes on the stargate controller block and tile entity to all clients
 			player.worldObj.markBlockForUpdate(this.x, this.y, this.z);
 		}
+	}
+	
+	/** Attempts to activate or deactivate the given stargate and the stargate it is in the process of dialing, returning whether or not it succeeded */
+	public boolean toggleStargatePair(EntityPlayer player,
+			TileEntityStargateController tileEntity) {
+		Triplet<Integer, Integer, Integer> decodedAddress = SDBlock.stargateController.decodeAddress(tileEntity.dialingAddress);
+		int dstDimension = decodedAddress.X;
+		// Get chunk coordinates
+		int dstChunkX = decodedAddress.Y;
+		int dstChunkZ = decodedAddress.Z;
+		int[] dstCoords = BlockStargateController.getDominantController(player.worldObj, dstChunkX, dstChunkZ);
+		if (dstCoords == null) {
+			return true;
+		}
+		TileEntityStargateController dstTileEntity = (TileEntityStargateController) player.worldObj.getTileEntity(dstCoords[0], dstCoords[1], dstCoords[2]);
+		
+		// Activate or deactivate the stargates if possible
+		if (dstCoords != null) {
+			switch (tileEntity.state) {
+			case READY:
+				SDSoundHandler.serverPlaySoundToPlayers(
+						player.worldObj.playerEntities, "stargate.kawoosh", 1F, 1F,
+						(double) tileEntity.xCoord, (double) tileEntity.yCoord, (double) tileEntity.zCoord);
+				SDBlock.stargateController.serverActivateStargatePair(
+						player.worldObj, tileEntity.xCoord, tileEntity.yCoord, tileEntity.zCoord,
+						dstCoords[0], dstCoords[1], dstCoords[2]);
+				break;
+			case ACTIVE_OUTGOING:
+				SDSoundHandler.serverPlaySoundToPlayers(
+						player.worldObj.playerEntities, "stargate.close", 1F, 1F,
+						(double) tileEntity.xCoord, (double) tileEntity.yCoord, (double) tileEntity.zCoord);
+				SDBlock.stargateController.serverDeactivateStargatePair(
+						player.worldObj, tileEntity.xCoord, tileEntity.yCoord, tileEntity.zCoord,
+						dstCoords[0], dstCoords[1], dstCoords[2]);
+				break;
+			default:
+			}
+		}
+		return false;
 	}
 }
