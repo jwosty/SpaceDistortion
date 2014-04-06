6a8a35a480a765a39ea5151dfc0d15970a73d7c6
jwosty <jwosty@users.noreply.github.com>
2014-04-05 20:53:19 -0600

    Changed version

--- src/main/java/jw/spacedistortion/common/CommonProxy.java
+++ src/main/java/jw/spacedistortion/common/CommonProxy.java
@@ -3,5 +3,5 @@ package jw.spacedistortion.common;
 public class CommonProxy {
 	public static final String MOD_ID = "spacedistortion";
 	public static final String MOD_NAME = "Space Distortion";
-	public static final String MOD_VERSION = "0.0.0";
+	public static final String MOD_VERSION = "0.5.0";
 }
