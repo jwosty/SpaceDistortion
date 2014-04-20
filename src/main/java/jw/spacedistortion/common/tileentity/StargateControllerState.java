10cdd51273d4b2863cf6c86891b0348ce41480b1
jwosty <jwosty@users.noreply.github.com>
2014-04-19 18:55:55 -0600

    More StargateControllerState refactoring

--- src/main/java/jw/spacedistortion/common/tileentity/StargateControllerState.java
+++ src/main/java/jw/spacedistortion/common/tileentity/StargateControllerState.java
@@ -1,10 +1,11 @@
 package jw.spacedistortion.common.tileentity;
 
+import jw.spacedistortion.common.block.Structure;
 import net.minecraft.nbt.NBTTagCompound;
 
 // Pretty much just emulating F#'s discriminated unions
 public abstract class StargateControllerState {	
-	private StargateControllerState() { }
+	protected StargateControllerState() { }
 	
 	public abstract float getGuiDisplayRed();
 	public abstract float getGuiDisplayGreen();
@@ -12,9 +13,34 @@ public abstract class StargateControllerState {
 	
 	public abstract String getType();
 	
+	/** Writes the state into an NBT tag */
 	public void writeToNBT(NBTTagCompound tag) {
 		String type = this.getType();
 		tag.setString("type", type);
+	}
+	
+	/** Reads the NBT tag into a state */
+	protected void readFromNBT(NBTTagCompound tag) { }
+	
+	/** Creates a stargate controller state based on the type string given */
+	private static StargateControllerState allocateStateType(String type) {
+		if (type.equals("invalid")) {
+			return new StargateControllerInvalid();
+		} else if (type.equals("ready")) {
+			return new StargateControllerReady();
+		} else if (type.equals("active")) {
+			return new StargateControllerActive();
+		} else {
+			String t = type == null ? "null" : "`" + type + "`";
+			throw new RuntimeException("Bad stargate controller state type; probably a bug! (got " + t + ")");
+		}
+	}
+	
+	/** Constructs a new stargate state from the NBT tag */
+	public static StargateControllerState createFromNBT(NBTTagCompound tag) {
+		StargateControllerState state = StargateControllerState.allocateStateType(tag.getString("type"));
+		state.readFromNBT(tag);
+		return state;
 	}
 	
 	/** Signifies a controller that isn't connected to a stargate ring */
@@ -47,6 +73,8 @@ public abstract class StargateControllerState {
 		public byte[] addressBuffer;
 		public int currentGlyphIndex;
 		
+		protected StargateControllerReady() { }
+		
 		public StargateControllerReady(byte[] dialingAddress, int currentGlyph) {
 			this.addressBuffer = dialingAddress;
 			this.currentGlyphIndex = currentGlyph;
@@ -62,6 +90,12 @@ public abstract class StargateControllerState {
 			super.writeToNBT(tag);
 			tag.setByteArray("address", this.addressBuffer);
 			tag.setInteger("glyph", this.currentGlyphIndex);
+		}
+		
+		@Override
+		protected void readFromNBT(NBTTagCompound tag) {
+			this.addressBuffer = tag.getByteArray("address");
+			this.currentGlyphIndex = tag.getInteger("glyph");
 		}
 
 		@Override
@@ -88,6 +122,8 @@ public abstract class StargateControllerState {
 		public int connectedZCoord;
 		public int connectedDimension;
 		
+		protected StargateControllerActive() { };
+		
 		public StargateControllerActive(boolean isIncoming, int connectedXCoord, int connectedYCoord,
 				int connectedZCoord, int connectedDimension) {
 			this.isOutgoing = isIncoming;
@@ -111,6 +147,15 @@ public abstract class StargateControllerState {
 			tag.setInteger("connz", this.connectedZCoord);
 			tag.setInteger("connd", this.connectedDimension);
 		}
+		
+		@Override
+		protected void readFromNBT(NBTTagCompound tag) {
+			this.isOutgoing = tag.getBoolean("incoming");
+			this.connectedXCoord = tag.getInteger("connx");
+			this.connectedYCoord = tag.getInteger("conny");
+			this.connectedZCoord = tag.getInteger("connz");
+			this.connectedDimension = tag.getInteger("connd");
+		}
 
 		@Override
 		public float getGuiDisplayRed() {
@@ -125,21 +170,6 @@ public abstract class StargateControllerState {
 		@Override
 		public float getGuiDisplayBlue() {
 			return this.isOutgoing ? 0f : 1f;
-		}
-	}
-	
-	public static StargateControllerState createFromNBT(NBTTagCompound tag) {
-		String type = tag.getString("type");
-		if (type.equals("invalid")) {
-			return new StargateControllerInvalid();
-		} else if (type.equals("ready")) {
-			return new StargateControllerReady(tag.getByteArray("address"), tag.getInteger("glyph"));
-		} else if (type.equals("active")) {
-			return new StargateControllerActive(tag.getBoolean("incoming"),
-					tag.getInteger("connx"), tag.getInteger("conny"), tag.getInteger("connz"), tag.getInteger("connd"));
-		} else {
-			String t = type == null ? "null" : "`" + type + "`";
-			throw new RuntimeException("Bad stargate controller state type; probably a bug! (got " + t + ")");
 		}
 	}
 }
