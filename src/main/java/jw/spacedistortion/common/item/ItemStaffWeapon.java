6a9b51eeb1247fb9d66d43e77c81abb9f7711015
jwosty <jwosty@users.noreply.github.com>
2014-09-27 14:19:44 -0600

    Added sound effect and improved third-person rendering (staff weapon)

--- src/main/java/jw/spacedistortion/common/item/ItemStaffWeapon.java
+++ src/main/java/jw/spacedistortion/common/item/ItemStaffWeapon.java
@@ -1,5 +1,10 @@
 package jw.spacedistortion.common.item;
 
+import org.lwjgl.opengl.GL11;
+
+import jw.spacedistortion.client.SDSoundHandler;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.audio.PositionedSound;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityLargeFireball;
 import net.minecraft.item.ItemStack;
@@ -11,6 +16,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 public class ItemStaffWeapon extends SDItem {
 	@Override
 	public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
+		stack.damageItem(10, player);
 		if (!world.isRemote) {
 			Vec3 look = player.getLookVec();
 			EntityLargeFireball fireball = new EntityLargeFireball(world, player, 1, 1, 1);
@@ -22,6 +28,10 @@ public class ItemStaffWeapon extends SDItem {
 			fireball.accelerationY = look.yCoord * 0.1;
 			fireball.accelerationZ = look.zCoord * 0.1;
 			world.spawnEntityInWorld(fireball);
+			
+			
+			SDSoundHandler.serverPlaySoundToPlayers(world.playerEntities, "weapons.staff", 1F, 0.9F + (itemRand.nextFloat() * 0.2F),
+						player.posX, player.posY, player.posZ);
 		}
 		return stack;
 	}
@@ -29,6 +39,8 @@ public class ItemStaffWeapon extends SDItem {
 	@Override
 	@SideOnly(Side.CLIENT)
 	public boolean isFull3D() {
+		GL11.glRotatef(-90, 1, 0, 0);
+		GL11.glTranslatef(0F, 0.25F, 0.4F);
 		return true;
 	}
 	
